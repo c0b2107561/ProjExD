43816a196e06b6136bc7e2f2a3ccaa7d7f15e336
hikapipo <c0b2107561@edu.teu.ac.jp>
2022-09-27 17:11:11 +0900

    時間計測まで完了

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,32 +1,33 @@
 import random
-import datetime
+import time
 
 num_of_alpha= 26 #全数
-num_of_all_chars = 10 #対象数
-num_of_abs_chars = 2 #欠損数
+num_of_all = 10 #対象数
+num_of_abs = 2 #欠損数
 num_of_challenge = 2 #チャレンジ数
 
 
 def shutudai(alphabet):
-    all_chars = random.sample(alphabet, num_of_all_chars)
+    all_taisyou = random.sample(alphabet, num_of_all)
     print("対象文字:",end="")
-    for c in sorted(all_chars):
+    for c in sorted(all_taisyou):
         print(c,end=" ")
     print()
 
-    abs_chars = random.sample(all_chars, num_of_abs_chars)
+    kesson = random.sample(all_taisyou, num_of_abs)
     print("表示文字:",end="")
-    for c in all_chars: 
-        if c not in abs_chars:
+    for c in all_taisyou: 
+        if c not in kesson:
             print(c, end=" ")
     print()
-    print("デバッグ用欠損文字:", abs_chars)
-    return abs_chars
+    print("デバッグ用欠損文字:", kesson)
+
+    return kesson
 
 
 def kaito(seikai):
     num=int(input("欠損文字はいくつあるでしょうか？:"))
-    if num != num_of_abs_chars:
+    if num != num_of_abs:
         print("不正解です")
     else:
         print("正解です。それでは、具体的に欠損文字一つずつ入力してください")
@@ -39,16 +40,24 @@ def kaito(seikai):
                 seikai.remove(c)
         else: 
             print("パーフェクトです！")
+            
             return True
     return False
+
+
 
 if __name__ == "__main__":
     alphabet = [chr(i+65) for i in range(num_of_alpha)]
     #shutudai(alphabet)
     for _ in range(num_of_challenge):
-        abs_chars = shutudai(alphabet)
-        ret = kaito(abs_chars)
+        time_sta = time.time()
+        time.sleep(1)
+        kesson = shutudai(alphabet)
+        ret = kaito(kesson)
         if ret:
+            time_end = time.time()
+            tim = time_end - time_sta
+            print(tim)
             break
         else:
             print("-"*20)
