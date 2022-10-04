0f268b977ceeef3c1dc6a750ee2a315fe797216d
hikapipo <c0b2107561@edu.teu.ac.jp>
2022-10-04 14:59:43 +0900

    練習問題６まで完了

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,13 +9,16 @@ def click_number(event):#練習3
 
 root = tk.Tk()
 root.title("練習問題")
-root.geometry("300x500")
+root.geometry("300x575")
 
-entry = tk.Entry(width=10, font=(",40"), justify="right")
+entry = tk.Entry(root, width=10, font=("",40), justify="right")
 entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0 #rは行、cは列を表す変数
-for i, num in enumerate(range(9,-1,-1),1):
+# for i, num in enumerate(list(range(9,-1,-1))+["+"],1): #無理くりまとめたやつ
+numbers = list(range(9, -1, -1)) #数字だけのリスト
+operators = ["+"] #演算子だけのリスト
+for i, num in enumerate(numbers+operators,1):
     btn = tk.Button(root, text=f"{num}", width=4, height=2, font=("Times New Roman", 30))
     btn.bind("<1>",click_number)
     btn.grid(row=r, column=c)
