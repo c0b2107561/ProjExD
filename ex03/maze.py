957697ff8f111f5e140cea04f26d04290d774cd4
hikapipo <c0b2107561@edu.teu.ac.jp>
2022-10-18 14:52:48 +0900

    練習03-10

--- ex03/maze.py
+++ ex03/maze.py
@@ -39,6 +39,10 @@ if __name__ == "__main__":
     root.bind("<KeyPress>",key_down) #練習5-2
     root.bind("<KeyRelease>", key_up) #練習6-2
 
-    main_proc()
+    main_proc() #練習7
+
+    maze_lst = mn.make_maze(15,9) #1:壁、0:床 #練習9
+
+    mn.show_maze(canvas, maze_lst)#練習10
 
     root.mainloop()
