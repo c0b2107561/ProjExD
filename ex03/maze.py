86c1441f1b27d5b7c44ace966cce35c0167bd7c9
hikapipo <c0b2107561@edu.teu.ac.jp>
2022-10-18 15:20:29 +0900

    練習03-11

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,16 +9,18 @@ def key_up(event): #練習6-1
     global key
     key = " "
 
-def main_proc(): #練習7
+def main_proc(): #練習7　#練習11
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -=20
+        my -=1
     if key == "Down":
-        cy +=20
+        my +=1
     if key == "Left":
-        cx -=20
+        mx -=1
     if key == "Right":
-        cx +=20
+        mx +=1
+    cx, cy = mx*100+50, my*100+50
 
     canvas.coords("tori", cx, cy)
     root.after(100, main_proc)
@@ -30,7 +32,13 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack() #練習2
 
+    maze_lst = mn.make_maze(15,9) #1:壁、0:床 #練習9
+
+    mn.show_maze(canvas, maze_lst)#練習10
+
+
     tori = tk.PhotoImage(file="ex03/fig/4.png")
+    mx, my = 1, 1 #練習11
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=tori, tag="tori") #練習3
 
@@ -41,8 +49,5 @@ if __name__ == "__main__":
 
     main_proc() #練習7
 
-    maze_lst = mn.make_maze(15,9) #1:壁、0:床 #練習9
-
-    mn.show_maze(canvas, maze_lst)#練習10
-
+    
     root.mainloop()
