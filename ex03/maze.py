058fd92b5ced48e5b47a2a00fb3fe63c8e1dcc29
hikapipo <c0b2107561@edu.teu.ac.jp>
2022-10-18 14:42:20 +0900

    練習03-7

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,6 +8,21 @@ def key_up(event): #練習6-1
     global key
     key = " "
 
+def main_proc(): #練習7
+    global cx, cy
+    if key == "Up":
+        cy -=20
+    if key == "Down":
+        cy +=20
+    if key == "Left":
+        cx -=20
+    if key == "Right":
+        cx +=20
+
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #練習1
@@ -22,4 +37,7 @@ if __name__ == "__main__":
 
     root.bind("<KeyPress>",key_down) #練習5-2
     root.bind("<KeyRelease>", key_up) #練習6-2
+
+    main_proc()
+
     root.mainloop()
