77e3b5f90cb66cf7bd25b37eba2d6c60cf6d8b36
hikapipo <c0b2107561@edu.teu.ac.jp>
2022-10-25 14:27:45 +0900

    練習問題04-3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -8,16 +8,24 @@ def main():
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg") #練習1
     bg_rct = bg_sfc.get_rect()
 
+    tori_sfc = pg.image.load("ex04/fig/6.png") #練習3
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect() 
+    tori_rct.center = 900,400 
+
     clock = pg.time.Clock() #練習1
 
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct) #練習2
-        pg.display.update()
 
         for event in pg.event.get(): #練習2
             if event.type == pg.QUIT:return
+        
+        scrn_sfc.blit(tori_sfc, tori_rct) #練習3
+        #blitの順番通りに表示される
 
-        clock.tick(0.5)
+        pg.display.update()
+        clock.tick(1000)
 
 
 
